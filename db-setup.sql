7cbe86ba05130b7a4a8edeb8f3538b3da14ebd29
Daniel To <danielto0252@gmail.com>
2012-11-02 12:10:56 -0700

    Update the db-setup script

--- db-setup.sql
+++ db-setup.sql
@@ -1,5 +1,6 @@
 create database clickr;
-create table FRquestions(Question_numb int NOT NULL AUTO_INCREMENT,
-						Question varchar(255) NOT NULL, 
-						Response varchar(255),
-						PRIMARY KEY (Question_numb));
+create table FRquestions(id int NOT NULL AUTO_INCREMENT,
+			Question_num int NOT NULL,
+			Question varchar(255) NOT NULL, 
+			Response varchar(255),
+			PRIMARY KEY (id));
