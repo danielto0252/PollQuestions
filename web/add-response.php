f979c0b2c8d4f1b55e01622fb274d4b4da696a34
Daniel To <danielto0252@gmail.com>
2012-11-25 09:40:47 -0800

    Create a php script to take post from iOS and add it to the db

--- web/add-response.php
+++ web/add-response.php
@@ -1,7 +1,24 @@
-<?php $handle = fopen('php://input','r');
-                $jsonInput = fgets($handle);
-                // Decoding JSON into an Array
-                $decoded = json_decode($jsonInput,true);
-                for($i = 0; $i < count($decoded); $i++)
-                	print "$i";
+<?php
+	$host = 'localhost';
+	$username = 'root';
+	$password = 'root';
+	$db = 'clickr';
+
+	$con = mysql_connect($host, $username, $password);
+	if (!$con)
+		die('Could not connect: ' . mysql_error());
+	
+	mysql_select_db($db);
+
+	$handle = fopen('php://input','r');
+	$jsonInput = fgets($handle);
+	$decoded = json_decode($jsonInput, true);
+	print_r($decoded);
+	if($decoded[response]){
+	echo $decoded[response];
+	echo $decoded[questionId];
+	$updateQuery = "UPDATE FRquestions SET Response = '$decoded[response]' WHERE id = '$decoded[questionId]'";
+	mysql_query($updateQuery);
+	}
+	mysql_close($con);
 ?>
