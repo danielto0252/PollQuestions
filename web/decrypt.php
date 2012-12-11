1bcd744bc3e5f5211525b6c7eccbba351b8327d8
Daniel To <danielto0252@gmail.com>
2012-12-10 20:32:52 -0500

    Try new decrypt script

--- web/decrypt.php
+++ web/decrypt.php
@@ -3,32 +3,20 @@
 $crypted = "GVW2Zd3Z0oeZEhhh4SghtA==";
 $pass = "chicken butt";
 
-print fnDecrypt($crypted, $pass);
+$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB);
+$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+$key = 'a16byteslongkey!a16byteslongkey!';
+$plaintext = "iphone";
 
-print decryptString($crypted, $pass);
+$ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $plaintext, MCRYPT_MODE_ECB);
+$base64encoded_ciphertext = base64_encode($ciphertext);
+echo "ciphertext: ".$base64encoded_ciphertext."<br/>";
 
-function fnDecrypt($sValue, $sSecretKey) {
-    return trim(
-        mcrypt_decrypt(
-            MCRYPT_RIJNDAEL_256, 
-            $sSecretKey, 
-            base64_decode($sValue), 
-            MCRYPT_MODE_ECB,
-            mcrypt_create_iv(
-                mcrypt_get_iv_size(
-                    MCRYPT_RIJNDAEL_256,
-                    MCRYPT_MODE_ECB
-                ), 
-                MCRYPT_RAND
-            )
-        )
-    );
-}
+$plaintext = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, base64_decode($base64encoded_ciphertext), MCRYPT_MODE_ECB);
+echo "plaintext: ".$plaintext."<br/>";
 
-
-function decyprtString($sValue, $sSecretKey) {
-	$padded_key = $sSecretKey . str_repeat(chr(0x00), 16);
-	$result = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $padded_key, base64_decode($sValue), 'ecb');
-}
+$base64encoded_ciphertext =  "I3chV+E2XUHeLCcJAhBaJQ==";
+$plaintext = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, base64_decode($base64encoded_ciphertext), MCRYPT_MODE_ECB);
+echo "plaintext: ".trim($plaintext);
 
 ?>
